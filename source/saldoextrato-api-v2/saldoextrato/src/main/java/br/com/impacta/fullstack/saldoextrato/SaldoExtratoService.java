6f5cd0fe60ff99678f29e34606c6085ff1c90833
Vinicius Branda Martinez <vinicius.b.martinez@gmail.com>
2021-01-20 08:53:31 -0300

    exercicio 4

--- source/saldoextrato-api-v2/saldoextrato/src/main/java/br/com/impacta/fullstack/saldoextrato/SaldoExtratoService.java
+++ source/saldoextrato-api-v2/saldoextrato/src/main/java/br/com/impacta/fullstack/saldoextrato/SaldoExtratoService.java
@@ -27,18 +27,14 @@ public class SaldoExtratoService {
         System.out.println("Creditos: " + creditoList);
         SaldoExtrato saldoExtrato = new SaldoExtrato();
         saldoExtrato.setCreditoList(creditoList);
-        BigDecimal creditoSum = creditoList.stream().map(Credito::getCredito).reduce(BigDecimal.ZERO, BigDecimal::add);
-        System.out.println("creditoSum: " + creditoSum);
         //Get Debito
         ResponseEntity<Debito[]> debitoResponse = restTemplate.getForEntity(DEBITO_API_URL, Debito[].class);
         System.out.println("DEBITO_API_URL: " + DEBITO_API_URL);
         List<Debito> debitoList = Arrays.asList(debitoResponse.getBody());
         System.out.println("Debitos: " + debitoList);
         saldoExtrato.setDebitoList(debitoList);
-        BigDecimal debitoSum = debitoList.stream().map(Debito::getDebito).reduce(BigDecimal.ZERO, BigDecimal::add);
-        System.out.println("debitoSum: " + debitoSum);
         //Calcular saldo
-        BigDecimal saldo = creditoSum.add(debitoSum);
+        BigDecimal saldo = calculateSaldo(creditoList, debitoList);
         saldoExtrato.setSaldo(saldo);
         System.out.println("saldo: " + saldo);
         return saldoExtrato;
@@ -51,21 +47,27 @@ public class SaldoExtratoService {
         System.out.println("CREDITO_API_URL: " + CREDITO_API_URL);
         List<Credito> creditoList = Arrays.asList(creditoResponse.getBody());
         System.out.println("Creditos: " + creditoList);
-        BigDecimal creditoSum = creditoList.stream().map(Credito::getCredito).reduce(BigDecimal.ZERO, BigDecimal::add);
-        System.out.println("creditoSum: " + creditoSum);
         //Get Debito
         ResponseEntity<Debito[]> debitoResponse = restTemplate.getForEntity(DEBITO_API_URL, Debito[].class);
         System.out.println("DEBITO_API_URL: " + DEBITO_API_URL);
         List<Debito> debitoList = Arrays.asList(debitoResponse.getBody());
         System.out.println("Debitos: " + debitoList);
-        BigDecimal debitoSum = debitoList.stream().map(Debito::getDebito).reduce(BigDecimal.ZERO, BigDecimal::add);
-        System.out.println("debitoSum: " + debitoSum);
         //Calcular saldo
         SaldoExtrato saldoExtrato = new SaldoExtrato();
-        BigDecimal saldo = creditoSum.add(debitoSum);
+        BigDecimal saldo = calculateSaldo(creditoList, debitoList);
         saldoExtrato.setSaldo(saldo);
         System.out.println("saldo: " + saldo);
         return saldoExtrato;
+    }
+
+    private BigDecimal calculateSaldo(List<Credito> creditoList, List<Debito> debitoList) {
+        BigDecimal creditoSum = creditoList.stream().map(Credito::getCredito).reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("creditoSum: " + creditoSum);
+        BigDecimal debitoSum = debitoList.stream().map(Debito::getDebito).reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("debitoSum: " + debitoSum);
+        BigDecimal saldo = creditoSum.add(debitoSum);
+        System.out.println("saldo: " + saldo);
+        return saldo;
     }
 
 }
